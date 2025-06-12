5f106e7ebd286ea657284adfe9184705ebf3441c
Jisu Park <susmisc14@naver.com>
2025-06-12 15:46:58 +0000

    Title: 아름다운 수, Time: 171ms, Memory: 15MB, Status: Passed - Codetree

--- 250612/아름다운 수/beautiful-number.js
+++ 250612/아름다운 수/beautiful-number.js
@@ -4,52 +4,30 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 
 // Please Write your code here.
+function solve(n) {
+    const count = (function recursive(currentLength) {
+        // Base Case 1: 목표 길이를 초과하면 유효하지 않은 경로이므로 0을 반환
+        if (currentLength > n) {
+            return 0;
+        }
 
-/**
- * 아이디어:
- * 1. n자리수 숫자 중 아름다운 수만 모은 배열 생성
- * 2. 아름다운 수는 `isButiful()`를 통해 확인
- *  2-1. `isButiful()`에서는 인자로 받은 숫자를 문자열화 한 뒤 연속된 문자를 그룹화 해서 배열에 저장
- *  2-2. 배열을 순회하면서 group의 요소들이 아름다운지 확인
- *  2-3. 하나라도 아름답지 않으면 그 수는 아름다운 수가 아님
- */
+        // Base Case 2: 목표 길이에 정확히 도달하면 유효한 조합 1개를 찾았으므로 1을 반환
+        if (currentLength === n) {
+            return 1;
+        }
 
-function isButiful(number) {
-    const stringified = number.toString();
-    let result = false;
+        // Recursive Step: 1, 22, 333, 4444를 붙이는 모든 경우의 수를 합산
+        // 'i'는 붙이려는 숫자를 의미하며, 곧 추가될 길이를 의미
+        let count = 0;
+        for (let i = 1; i <= 4; i++) {
+            count += recursive(currentLength + i);
+        }
 
-    if (/[056789]/.test(stringified)) {
-        return result;
-    }
+        return count;
+    })(0);
 
-    const group = stringified.match(/(.)\1*/g);
-    for (const item of group) {
-        const first = parseInt(item[0], 10);
-        if (item.length % first !== 0) {
-            result = false;
-            break;
-        };
-
-        result = true;
-    }
-
-    return result;
+    return count;
 }
 
-function getBeautifulNumbers(n) {
-    const start = Math.pow(10, n - 1);
-    const end = Math.pow(10, n) - 1;
-
-    const numbers = [];
-    for (let i = start; i <= end; i++) {
-        if (!isButiful(i)) continue;
-        numbers.push(i);
-    }
-
-    return numbers;
-}
-
-// 풀이
-const numbers = getBeautifulNumbers(n)
-
-console.log(numbers.length);
+const result = solve(n);
+console.log(result);
