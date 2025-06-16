3d5958e555e865d52ffe8b56d176b6cf53267430
Jisu Park <susmisc14@naver.com>
2025-06-16 19:11:43 +0900

    Title: 최소 경로로 탈출 하기, Time: 5007ms, Memory: 19MB, Status: Time Limit Exceed - Codetree

--- 250616/최소 경로로 탈출 하기/escape-with-min-distance.js
+++ 250616/최소 경로로 탈출 하기/escape-with-min-distance.js
@@ -7,42 +7,42 @@ const grid = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
 // Please Write your code here.
 function solve(n, m, grid) {
     const visited = Array.from({ length: n }, () => Array.from({ length: m }).fill(false));
-    const queue = [];
-
+    
     // 시계 방향
     const dx = [1, 0, -1, 0];
     const dy = [0, 1, 0, -1];
 
-    // 1. 큐에 시작점 정보 [y, x, distance]를 추가
-    queue.push([0, 0, 0]);
-    visited[0][0] = true;
-
-    // 2. 큐가 빌 때까지 반복
-    while (queue.length > 0) {
-        const [y, x, distance] = queue.shift();
-
-        // 3. 도착지에 도달했으면 현재 거리 반환
+    const minPath = (function recursive(y, x) {
         if (y === n - 1 && x === m - 1) {
-            return distance;
+            return 0;
         }
 
-        // 4. 시계 방향으로 인접한 칸 탐색
+        let length = Number.MAX_SAFE_INTEGER;
+
+        visited[y][x] = true;
+
         for (let i = 0; i < 4; i++) {
             const ny = y + dy[i];
             const nx = x + dx[i];
 
-            // 5. 다음 칸이 유효한지 확인
-            if (ny >= 0 && ny < n && nx >= 0 && nx < m && grid[ny][nx] === 1 && !visited[ny][nx]) {
-                visited[ny][nx] = true;
-                
-                // 6. 유효하면 다음 탐색을 위해 큐에 추가
-                queue.push([ny, nx, distance + 1]);
+            // 그리드 범위를 벗어났을 떄
+            if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+
+            // 이미 방문한 경로일 때
+            if (visited[ny][nx]) continue;
+
+            // 뱀이 없을 경우
+            if (grid[ny][nx] === 1) {
+                length = Math.min(recursive(ny, nx) + 1, length);
             }
         }
-    }
 
-    // 큐가 모두 비었는데 도달하지 못했다면 길이 없는 것
-    return -1;
+        visited[y][x] = false;
+
+        return length;
+    })(0, 0);
+
+    return minPath !== Number.MAX_SAFE_INTEGER ? minPath : -1;
 }
 
 console.log(solve(n, m, grid));
