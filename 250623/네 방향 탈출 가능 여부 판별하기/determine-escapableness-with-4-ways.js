1323fe8cd5de11107b9582eebaa97339ed077dc2
Jisu Park <susmisc14@naver.com>
2025-06-23 00:22:59 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 189ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250623/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const grid = input.slice(1, n + 1).map(line => line.trim().split(' ').map(Number));
+
+// Please Write your code here.
+const queue = [[0, 0]];
+const visited = Array.from({ length: n }, () => new Array(m).fill(false));
+let result = 0;
+
+// 시계 방향
+const dx = [1, 0, -1, 0];
+const dy = [0, 1, 0, -1];
+
+visited[0][0] = true;
+
+while (queue.length > 0) {
+    const [x, y] = queue.shift();
+
+    if (x === m - 1 && y === n - 1) {
+        result = 1;
+        break;
+    }
+
+    
+    for (let i = 0; i < 4; i++) {
+        const nx = x + dx[i];
+        const ny = y + dy[i];
+
+        if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+
+        if (grid[ny][nx] === 1 && !visited[ny][nx]) {
+            queue.push([nx, ny]);
+            visited[ny][nx] = true;   
+        };
+    }
+}
+
+console.log(result);
