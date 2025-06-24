f1b5ed4ac185301b85509d3faccd30f408a55cf5
Jisu Park <susmisc14@naver.com>
2025-06-24 13:18:05 +0900

    Title: 2차원 폭발 게임, Time: 671ms, Memory: 31MB, Status: Passed - Codetree

--- 250624/2차원 폭발 게임/The-2D-bomb-game.js
+++ 250624/2차원 폭발 게임/The-2D-bomb-game.js
@@ -7,19 +7,33 @@ const grid = input.slice(1, Number(n) + 1).map(line => line.trim().split(' ').ma
 // Please Write your code here.
 let currentGrid = grid.map((row) => [...row]);
 
-let i = 0;
-while (i <= k) {
+for (let i = 0; i < k; i++) {
+    while (true) {
+        const [newGrid, exploded] = explode(currentGrid);
+
+        if (!exploded) break;
+
+        currentGrid = drop(newGrid);
+    }
+
     currentGrid = rotate(currentGrid);
     currentGrid = move(currentGrid);
     currentGrid = drop(currentGrid);
-    i++;
+}
+
+while (true) {
+    const [newGrid, exploded] = explode(currentGrid);
+
+    if (!exploded) break;
+
+    currentGrid = drop(newGrid);
 }
 
 let result = 0;
 for (let r = 0; r < n; r++) {
     for (let c = 0; c < n; c++) {
         if (currentGrid[r][c] === 0) continue;
-
+        
         result++;
     }
 }
@@ -27,67 +41,61 @@ for (let r = 0; r < n; r++) {
 console.log(result);
 
 // helpers
-function rotate (grid) {
+function explode(grid) {
+    const newGrid = grid.map((row) => [...row]);
+    let exploded = false;
+
+    for (let c = 0; c < n; c++) {
+        for (let r = 0; r < n; r++) {
+            if (newGrid[r][c] === 0) continue;
+
+            let count = 0;
+            for (let i = r; i < n; i++) {
+                if (newGrid[i][c] === newGrid[r][c]) {
+                    count++;
+                } else {
+                    break;
+                }
+            }
+
+            if (count >= m) {
+                for (let i = r; i < r + count; i++) {
+                    newGrid[i][c] = 0;
+                }
+                
+                exploded = true;
+            }
+        }
+    }
+
+    return [newGrid, exploded];
+}
+
+function drop(grid) {
+    const newGrid = Array.from({ length: n }, () => new Array(n).fill(0));
+
+    for (let c = 0; c < n; c++) {
+        let keepRow = n - 1;
+
+        for (let r = n - 1; r >= 0; r--) {
+            if (grid[r][c] !== 0) {
+                newGrid[keepRow][c] = grid[r][c];
+                keepRow--;
+            }
+        }
+    }
+
+    return newGrid;
+}
+
+function rotate(grid) {
     const newGrid = Array.from({ length: n }, () => new Array(n).fill(0));
 
     for (let r = 0; r < n; r++) {
         for (let c = 0; c < n; c++) {
-            newGrid[c][n - r - 1] = grid[r][c];
+            newGrid[c][n - 1 - r] = grid[r][c];
         }
     }
 
     return newGrid;
-}
-
-function move(grid) {
-    const newGrid = [];
-
-    for (let r = 0; r < n; r++) {
-        const filteredRow = grid[r].filter((current) => current !== 0);
-        const newRow = [];
-        let count = 1;
-
-        for (let i = 1; i <= filteredRow.length; i++) {
-            if (i < filteredRow.length && filteredRow[i] === filteredRow[i - 1]) {
-                count++;
-            } else {
-                if (count < m) {
-                    newRow.push(...Array(count).fill(filteredRow[i - 1]));
-                }
-
-                count = 1;
-            }
-        }
-
-        while (newRow.length < n) {
-            newRow.push(0);
-        }
-
-        newGrid.push(newRow);
-    }
-
-    return newGrid;
-}
-
-function drop(grid) {
-    const clonedGrid = grid.map((row) => [...row]);
-
-    for (let c = 0; c < n; c++) {
-        let keepRow = null;
-
-        for (let r = n - 1; r >= 0; r--) {
-            if (clonedGrid[r][c] === 0) {
-                keepRow = Math.max(r, keepRow);
-                continue;
-            }
-            
-            if (keepRow !== null) {
-                clonedGrid[keepRow][c] = clonedGrid[r][c];
-                clonedGrid[r][c] = 0;
-                keepRow = r;
-            }
-        }
-    }
-
-    return clonedGrid;
 }
