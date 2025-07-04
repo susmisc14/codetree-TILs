fe550c896ac75b170ae7c6af8deb1f6f2b9429d1
Jisu Park <su.smisc14@gmail.com>
2025-07-04 21:19:27 +0900

    Title: 우리는 하나, Time: 169ms, Memory: 15MB, Status: Passed - Codetree

--- 250704/우리는 하나/we-are-the-one.js
+++ 250704/우리는 하나/we-are-the-one.js
@@ -6,46 +6,55 @@ const grid = input.slice(1, 1 + N).map(line => line.split(' ').map(Number));
 
 // Please Write your code here.
 function solve() {
-    const result = (function recursive(cities) {
-        if (cities.length === K) {
-            const visited = Array.from({ length: N }, () => new Array(N).fill(false));
+    const coords = [];
+    for (let row = 0; row < N; row++) {
+        for (let col = 0; col < N; col++) {
+            coords.push([row, col]);
+        }
+    }
 
-            return cities.reduce((acc, current) => acc + countMove(current, visited), 0);
+    const result = (function recursive(index, cities) {
+        if (cities.length === K) {
+            const count = countReachableCities(cities);
+
+            return count;
         }
 
         let maxCount = 0;
 
-        for (let row = 0; row < N; row++) {
-            for (let col = 0; col < N; col++) {
-                maxCount = Math.max(recursive([...cities, [row, col]]), maxCount);
-            }
+        for (let i = index; i < coords.length; i++) {
+            maxCount = Math.max(recursive(index + 1, [...cities, coords[i]]), maxCount);
         }
 
         return maxCount;
-    })([]);
+    })(0, []);
 
     return result;
 }
 
 console.log(solve());
+// solve()
 
 // helpers
-function countMove(startPosition, visited) {
-    const queue = [startPosition];
+function countReachableCities(cities) {
+    const visited = Array.from({ length: N }, () => new Array(N).fill(false));
+    const queue = [];
     let head = 0;
 
-    visited[startPosition[0]][startPosition[1]] = true;
+    for (const [row, col] of cities) {
+        if (!visited[row][col]) {
+            queue.push([row, col]);
+            visited[row][col] = true;
+        }
+    }
 
     // 시계 방향
     const dr = [0, 1, 0, -1];
     const dc = [1, 0, -1, 0];
 
-    let count = 0;
-
-    while(head < queue.length) {
+    while (head < queue.length) {
         const [currentRow, currentCol] = queue[head];
         head += 1;
-        count += 1;
 
         for (let i = 0; i < 4; i++) {
             const nextRow = currentRow + dr[i];
@@ -53,18 +62,19 @@ function countMove(startPosition, visited) {
 
             if (!inRange(nextRow, nextCol)) continue;
 
-            if (!visited[nextRow][nextCol] && canMove(grid[currentRow][currentCol], grid[nextRow][nextCol])) {
+            if (!visited[nextRow][nextCol] && canMove(currentRow, currentCol, nextRow, nextCol)) {
                 queue.push([nextRow, nextCol]);
                 visited[nextRow][nextCol] = true;
             }
         }
     }
 
-    return count;
+    return head;
 }
 
-function canMove(a, b) {
-    const diff = Math.abs(a - b);
+function canMove(row1, col1, row2, col2) {
+    const diff = Math.abs(grid[row1][col1] - grid[row2][col2]);
+
     return diff >= U && diff <= D;
 }
 
