4b012588cdd10d5933444280b12a4d5c910f6f55
Jisu Park <su.smisc14@gmail.com>
2025-07-20 15:12:03 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 214ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250720/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const N = Number(input[0]);
+const segments = input.slice(1, N + 1).map(line => line.split(' ').map(Number));
+
+// Please Write your code here.
+function solve() {
+    const dp = new Array(N).fill(0);
+    dp[0] = 1;
+
+    for (let i = 1; i < N; i++) {
+        for (let j = 0; j < i; j++) {
+            if (isOverlap(segments[j], segments[i])) continue;
+
+            dp[i] = Math.max(dp[j] + 1, dp[i]);
+        }
+    }
+
+    return Math.max(...dp);
+}
+
+console.log(solve());
+
+// helpers
+function isOverlap(a, b) {
+    return a[0] >= b[0] && a[0] <= b[1] || a[1] >= b[0] && a[1] <= b[1];
+}
