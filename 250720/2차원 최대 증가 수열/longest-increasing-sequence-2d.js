1ea4ca4446bbe6cac1dac873f467285e0c558307
Jisu Park <su.smisc14@gmail.com>
2025-07-20 12:07:30 +0900

    Title: 2차원 최대 증가 수열, Time: 172ms, Memory: 15MB, Status: Passed - Codetree

--- 250720/2차원 최대 증가 수열/longest-increasing-sequence-2d.js
+++ 250720/2차원 최대 증가 수열/longest-increasing-sequence-2d.js
@@ -6,14 +6,15 @@ const grid = input.slice(1, N + 1).map(row => row.trim().split(' ').map(Number))
 
 // Please Write your code here.
 function solve() {
-    const dp = Array.from({ length: N }, () => new Array(M).fill(1));
+    const dp = Array.from({ length: N }, () => new Array(M).fill(0));
+    dp[0][0] = 1;
 
     for (let row1 = 0; row1 < N; row1++) {
         for (let col1 = 0; col1 < M; col1++) {
             for (let row2 = 0; row2 < row1; row2++) {
                 for (let col2 = 0; col2 < col1; col2++) {
-                    if (grid[row2][col2] >= grid[row1][col1]) continue;
-                    
+                    if (grid[row2][col2] >= grid[row1][col1] || dp[row2][col2] === 0) continue;
+
                     dp[row1][col1] = Math.max(dp[row1][col1], dp[row2][col2] + 1);
                 }
             }
