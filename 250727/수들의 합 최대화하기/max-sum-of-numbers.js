3f1739bc2f5e188a52a7178dee08f2c9137d0876
Jisu Park <su.smisc14@gmail.com>
2025-07-27 20:57:45 +0900

    Title: 수들의 합 최대화하기, Time: 219ms, Memory: 18MB, Status: Passed - Codetree

--- 250727/수들의 합 최대화하기/max-sum-of-numbers.js
+++ 250727/수들의 합 최대화하기/max-sum-of-numbers.js
@@ -6,32 +6,25 @@ const grid = input.slice(1, N + 1).map(line => line.split(' ').map(Number));
 
 // Please Write your code here.
 function solve() {
-    const visited = {
-        row: new Array(N).fill(false),
-        col: new Array(N).fill(false)
-    };
+    const visited = new Array(N).fill(false);
     
-    const result = (function recursive(coords) {
-        if (coords.length === N) {
+    const result = (function recursive(row, coords) {
+        if (row === N) {
             return coords.reduce((acc, [row, col]) => acc + grid[row][col], 0);
         }
 
         let maxNumber = -Infinity;
 
-        for (let row = 0; row < N; row++) {
-            for (let col = 0; col < N; col++) {
-                if (visited.row[row] || visited.col[col]) continue;
-
-                visited.row[row] = true;
-                visited.col[col] = true;
-                maxNumber = Math.max(recursive([...coords, [row, col]]), maxNumber);
-                visited.row[row] = false;
-                visited.col[col] = false;
+        for (let col = 0; col < N; col++) {
+            if (!visited[col]) {
+                visited[col] = true;
+                maxNumber = Math.max(recursive(row + 1, [...coords, [row, col]]), maxNumber);
+                visited[col] = false;
             }
         }
 
         return maxNumber;
-    })([]);
+    })(0, []);
 
     return result;
 }
