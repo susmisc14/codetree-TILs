7f7f9f224b9f4f15bfe6ff1954dc870e9ce0700f
Jisu Park <su.smisc14@gmail.com>
2025-07-30 18:09:27 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 378ms, Memory: 18MB, Status: Passed - Codetree

---
+++ 250730/수들 중 최솟값 최대화하기/maximin-of-numbers.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const N = Number(input[0]);
+const grid = input.slice(1, N + 1).map(line => line.split(" ").map(Number));
+
+// Please Write your code here.
+function solve() {
+    const visited = new Array(N).fill(false);
+
+    const result = (function recursive(row, numbers) {
+        if (row === N) {
+            return Math.min(...numbers);
+        }
+
+        let maxNumber = -Infinity;
+
+        for (let col = 0; col < N; col++) {
+            if (!visited[col]) {
+                visited[col] = true;
+                maxNumber = Math.max(recursive(row + 1, [...numbers, grid[row][col]]), maxNumber);
+                visited[col] = false;
+            }
+        }
+
+        return maxNumber;
+    })(0, []);
+
+    return result;
+}
+
+console.log(solve());
