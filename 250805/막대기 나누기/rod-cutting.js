2fca8b786a9b53d6053bead837f4dd6fef5f5a69
Jisu Park <su.smisc14@gmail.com>
2025-08-05 23:20:29 +0900

    Title: 막대기 나누기, Time: 175ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250805/막대기 나누기/rod-cutting.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const N = Number(input[0]);
+const profit = input[1].split(" ").map(Number);
+
+// Please Write your code here.
+function solve() {
+    const dp = new Array(N + 1).fill(-Infinity);
+    dp[0] = 0;
+
+    for (let i = 1; i <= N; i++) {
+        for (let j = 1; j <= i; j++) {
+            dp[i] = Math.max(profit[j - 1] + dp[i - j], dp[i]);
+        }
+    }
+
+    return dp[N];
+}
+
+console.log(solve());
