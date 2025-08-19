2e693abcaa470d6ff46c9a1773fd90009bccbfe3
Jisu Park <su.smisc14@gmail.com>
2025-08-20 08:07:09 +0900

    Title: 막대기 나누기, Time: 176ms, Memory: 15MB, Status: Passed - Codetree

--- 250820/막대기 나누기/rod-cutting.js
+++ 250820/막대기 나누기/rod-cutting.js
@@ -5,11 +5,15 @@ const N = Number(input[0]);
 const profit = input[1].split(" ").map(Number);
 
 // Please Write your code here.
-const dp = new Array(N + 1).fill(-Infinity);
-dp[0] = 0;
+const dp = new Array(N + 1).fill(0);
 
+// 바깥 루프: 목표로 하는 막대의 길이 (1부터 N까지)
 for (let i = 1; i <= N; i++) {
+
+    // 안쪽 루프: 첫 번째로 자를 조각의 길이 (1부터 현재 막대 길이까지)
     for (let j = 1; j <= i; j++) {
+        // 점화식: 
+        // 현재 최대 가치 vs (첫 조각의 가치 + 나머지 부분의 최대 가치)
         dp[i] = Math.max(profit[j - 1] + dp[i - j], dp[i]);
     }
 }
