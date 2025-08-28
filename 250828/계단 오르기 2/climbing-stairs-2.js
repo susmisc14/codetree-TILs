84370726345ecf115a51dbff05eb6bf72ce3a640
Jisu Park <su.smisc14@gmail.com>
2025-08-28 14:09:44 +0900

    Title: 계단 오르기 2, Time: 188ms, Memory: 15MB, Status: Passed - Codetree

--- 250828/계단 오르기 2/climbing-stairs-2.js
+++ 250828/계단 오르기 2/climbing-stairs-2.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split("\n");
 
 const N = Number(input[0]);
-const coins = input[1].split(" ").map(Number);
+const coins = input[1].trim().split(" ").map(Number);
 
 // Please Write your code here.
 const COUNT = 3;
@@ -13,14 +13,6 @@ dp[0][0] = 0;
 for (let i = 1; i <= N; i++) {
     for (let j = 0; j <= COUNT; j++) {
         const currentCoin = coins[i - 1];
-
-        if (i === N) {
-            if (j > 0 && dp[i - 1][j - 1] !== -Infinity) {
-                dp[i][j] = dp[i - 1][j - 1] + currentCoin;
-            }
-            continue;
-        }
-
         let valueFromOneStep = -Infinity;
         let valueFromTwoStep = -Infinity;
 
